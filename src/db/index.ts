f89d5fa6d04fbf6a7573e38972ceb874786981c9
JChicano <jchicano43@gmail.com>
2024-03-13 11:25:46 +0100

    inicio conexion con turso

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,10 @@
+import { createClient } from "@libsql/client/.";
 import { Client } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
-
-const client = new Client({
-  url: process.env.DATABASE_URL,
-});
+// const client = new Client({
+//   url: process.env.DATABASE_URL,
+// });
+const client = createClient ({url: process.env.DATABASE_URL!, authToken: process.env.DATABASE_AUTH_TOKEN!});
 
 export const db = drizzle(client);
+
