d20a01740e52c6834292b374bddc38a387f50fbc
Javier <jchicano43@gmail.com>
2024-05-13 12:23:28 +0200

    ajustado seccion info con scroll

--- src/app/components/main/seccionInfo.tsx
+++ src/app/components/main/seccionInfo.tsx
@@ -1,39 +1,59 @@
-"use client"
+"use client";
+import { useEffect, useState } from "react";
 import DivInfo from "./divInfo";
+import { useInView } from "react-intersection-observer";
 
 export default function SeccionInfo() {
+  const [isVisible, setIsVisible] = useState(false);
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setIsVisible(true);
+    }
+  }, [inView]);
   return (
-    <section className="flex gap-10 w-3/4 justify-around border-2 border-colorBase p-10 mb-32">
-      <DivInfo
-        datos={{
-          numero: 29,
-          texto: "Modelos de mesas",
-        }}
-      />
-      <DivInfo
-        datos={{
-          numero: 31,
-          texto: "Modelos de sillas",
-        }}
-      />
-      <DivInfo
-        datos={{
-          numero: 5,
-          texto: "Modelos de bancos",
-        }}
-      />
-      <DivInfo
-        datos={{
-          numero: 9,
-          texto: "Materiales diferentes",
-        }}
-      /> 
-      <DivInfo
-        datos={{
-          numero: 266,
-          texto: "Colores diferentes",
-        }}
-      />
+    <section
+      className="flex gap-10 w-3/4 justify-around border-2 border-colorBase p-10 mb-32"
+      ref={ref}
+    >
+      {isVisible && (
+        <>
+          <DivInfo
+            datos={{
+              numero: 29,
+              texto: "Modelos de mesas",
+            }}
+          />
+          <DivInfo
+            datos={{
+              numero: 31,
+              texto: "Modelos de sillas",
+            }}
+          />
+          <DivInfo
+            datos={{
+              numero: 5,
+              texto: "Modelos de bancos",
+            }}
+          />
+          <DivInfo
+            datos={{
+              numero: 9,
+              texto: "Materiales diferentes",
+            }}
+          />
+          <DivInfo
+            datos={{
+              numero: 266,
+              texto: "Colores diferentes",
+            }}
+          />
+        </>
+      )}
     </section>
   );
 }
