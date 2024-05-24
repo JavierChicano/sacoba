6e4c0b31b43dfc31214514de348810966de53596
JChicano <jchicano43@gmail.com>
2024-05-24 21:07:36 +0200

    setup de resend

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,24 @@
+import { Resend } from 'resend';
+import { EmailTemplate } from '@/components/email-template';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['jchicano43@gmail.com'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'John' }),
+      text: 'Welcome, John!',
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
