f97b79e99a3ca7da5b5adf778594d94a893e789b
JChicano <jchicano43@gmail.com>
2024-12-17 18:36:27 +0100

    script Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ import CookieConsentimiento from "./components/cookieConsentimiento";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Muebles sacoba",
-  description: "Pagina web de venta al público de mesas, sillas y bancos de cocina",
+  title: "Muebles Sacoba",
+  description: "Página web de venta al público de mesas, sillas y bancos de cocina",
 };
 
 export default function RootLayout({
@@ -20,14 +20,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-D7F98HXQYR"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', 'G-D7F98HXQYR');
+            `,
+          }}
+        />
+      </head>
       <body className={cn("bg-fondo min-h-lvh w-full", inter.className)}>
         <ClientComponent>
           <Header />
           {children}
-          <CookieConsentimiento/>
+          <CookieConsentimiento />
           <Footer />
         </ClientComponent>
       </body>
